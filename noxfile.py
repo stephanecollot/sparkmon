1475149b59c9ffb93fecaa067a5ebeee6f4c2130
Stephane Collot <stephane.collot@ing.com>
2021-07-02 21:08:18 +0200

    pre commit

--- noxfile.py
+++ noxfile.py
@@ -84,7 +84,7 @@ def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
         hook.write_text("\n".join(lines))
 
 
-@session(name="pre-commit", python="3.9")
+@session(name="pre-commit", python="3.7")
 def precommit(session: Session) -> None:
     """Lint using pre-commit."""
     args = session.posargs or ["run", "--all-files", "--show-diff-on-failure"]
