db71fb0708a9a9560ed4a299315539504f0f1038
Stephane Collot <stephane.collot@ing.com>
2022-03-09 13:56:53 +0100

    remove safety check

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ python_versions = ["3.9", "3.8", "3.7", "3.6"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
-    "safety",
+    # "safety",
     # "mypy",
     "tests",
     "typeguard",
